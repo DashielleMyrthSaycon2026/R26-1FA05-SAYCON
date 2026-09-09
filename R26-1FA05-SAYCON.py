c6877d634f53615e8f565f9a38f79b8da7c5b977
DashielleMyrthSaycon2026 <dmcsaycon@cmc.pshs.edu.ph>
2026-09-09 09:36:17 +0800

    Add files via upload

---
+++ R26-1FA05-SAYCON.py
@@ -0,0 +1,7 @@
+x1 = float(int(input("Enter x1: ")))
+y1 = float(int(input("Enter x1: ")))
+x2 = float(int(input("Enter x1: ")))
+y2 = float(int(input("Enter x1: ")))
+
+d = x1, x2, y1, y2
+print(sqrt((pow((x2, x1)2) + pow((y2, y1)2))))
