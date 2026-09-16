b7fc877ce1974d3cdf90a5f598183cddc22abe43
DashielleMyrthSaycon2026 <dmcsaycon@cmc.pshs.edu.ph>
2026-09-16 09:32:31 +0800

    Update R26-1FA05-SAYCON.py

--- R26-1FA05-SAYCON.py
+++ R26-1FA05-SAYCON.py
@@ -1,7 +1,14 @@
+import math # To be able to calculate the distance and the formula sqrt and pow
 x1 = float(int(input("Enter x1: ")))
 y1 = float(int(input("Enter x1: ")))
 x2 = float(int(input("Enter x1: ")))
 y2 = float(int(input("Enter x1: ")))
 
+# Calculate the distance
 d = x1, x2, y1, y2
-print(sqrt((pow((x2, x1)2) + pow((y2, y1)2))))
+print(math.sqrt((math.pow((x2 - x2),2) + math.pow((y2 - y1),2))))
+
+# Reflection: Using a library was more practical and made the code easier to make.
+# 1. It had many shortcuts.
+# 2. sqrt() and pow()
+# 3. The code would be too long and complicated.
